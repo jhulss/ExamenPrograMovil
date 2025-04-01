64522c30458fe5394380d11e5808c3580d7bb7c9
calyr <calyr.software@gmail.com>
2025-03-31 20:57:15 -0400

    fixed codesmells

--- framework/src/main/java/com/ucb/framework/service/IMovieApiService.kt
+++ framework/src/main/java/com/ucb/framework/service/IMovieApiService.kt
@@ -1,10 +1,8 @@
 package com.ucb.framework.service
 
-import com.ucb.framework.dto.AvatarResponseDto
 import com.ucb.framework.dto.MovieResponseDto
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface IMovieApiService {
